b69125190c7c26d98b93ebd044a04bc5b2a2ba02
Ramon Voges <r.voges@dnb.de>
2024-04-29 10:28:41 +0200

    Ollama für lokale Ausführung ergänzen

--- bot/chatbot-st.py
+++ bot/chatbot-st.py
@@ -1,6 +1,7 @@
 import os
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings, StorageContext, load_index_from_storage
-from llama_index.llms.perplexity import Perplexity
+# from llama_index.llms.perplexity import Perplexity
+from llama_index.llms.ollama import Ollama
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 import streamlit as st
 
@@ -8,12 +9,13 @@ import streamlit as st
 st.title("DNB-ChatBot")
 
 # Laden des Perplexity API-Key (in der shell anlegen mit: export PERPLEXITY_API_KEY=...)
-pplx_api_key = os.environ["PERPLEXITY_API_KEY"]
+# pplx_api_key = os.environ["PERPLEXITY_API_KEY"]
 
 # Erstellen einer Instanz von Perplexity LLM
 # llm = Perplexity(api_key=pplx_api_key, model="sonar-small-chat", temperature=0.5, system_prompt="Du bist ein Experte für die Deutsche Nationalbibliothek. Du hilfst Nutzerinnen und Nutzern dabei, die Bibliothek zu benutzen. Du beantwortest Fragen zum Ausleihbetrieb und den verfügbaren Services. Deine Antworten sollen auf Fakten basieren. Halluziniere keine Informationen über die Bibliothek, die nicht auf Fakten basieren. Wenn Du eine Information über die Bibliothek nicht hast, sage den Nutzenden, dass Du Ihnen nicht weiterhelfen kannst. Antworte auf Deutsch.")
 # llm = Perplexity(api_key=pplx_api_key, model="sonar-small-chat", temperature=0.5, system_prompt="Du bist ein Experte für die Briefe von Christian Felix Weiße, die von Mark Lehmstedt unter Mitarbeit von Katrin Löffler herausgegeben und ediert wurden. Du hilfst Nutzerinnen und Nutzern dabei, Informationen über diese Briefe zu erhalten. Du beantwortest Fragen zum Inhalt der Briefe. Jeder Brief ist nummeriert. Zu Beginn jedes Eintrags steht, an wen der Brief Adressiert ist, und, wenn möglich, wann und wo er von Weiße geschrieben wurde. Deine Antworten sollen auf Fakten basieren. Halluziniere keine Informationen über die Briefe, die nicht auf Fakten basieren. Wenn Du eine Information in den Briefen nicht findest, sage den Nutzenden, dass Du Ihnen nicht weiterhelfen kannst. Antworte auf Deutsch.")
-llm = Perplexity(api_key=pplx_api_key, model="sonar-small-chat", temperature=0.5, system_prompt="Du bist ein Experte für die Erschließung von Schlagworten. Du hilfst den Erschließenden dabei, die richtigen Schlagworte zu finden. Du beantwortest dafür Fragen auf der Grundlage der Regeln für die Schlagwortkategorisierung. Deine Antworten sollen auf den Regeln basieren. Halluziniere keine Informationen über die Schlagwortkategorisierung, die nicht auf den Regeln basieren. Wenn Du eine Information über die Schlagwortkategorisierung nicht hast, sage den Erschließenden, dass Du Ihnen nicht weiterhelfen kannst. Antworte auf Deutsch.")
+# llm = Perplexity(api_key=pplx_api_key, model="sonar-small-chat", temperature=0.5, system_prompt="Du bist ein Experte für die Erschließung von Schlagworten. Du hilfst den Erschließenden dabei, die richtigen Schlagworte zu finden. Du beantwortest dafür Fragen auf der Grundlage der Regeln für die Schlagwortkategorisierung. Deine Antworten sollen auf den Regeln basieren. Halluziniere keine Informationen über die Schlagwortkategorisierung, die nicht auf den Regeln basieren. Wenn Du eine Information über die Schlagwortkategorisierung nicht hast, sage den Erschließenden, dass Du Ihnen nicht weiterhelfen kannst. Antworte auf Deutsch.")
+llm = Ollama(model="phi3", request_timeout=120.0, system_prompt="Du bist ein Experte für die Erschließung von Schlagworten. Du hilfst den Erschließenden dabei, die richtigen Schlagworte zu finden. Du beantwortest dafür Fragen auf der Grundlage der Regeln für die Schlagwortkategorisierung. Deine Antworten sollen auf den Regeln basieren. Halluziniere keine Informationen über die Schlagwortkategorisierung, die nicht auf den Regeln basieren. Wenn Du eine Information über die Schlagwortkategorisierung nicht hast, sage den Erschließenden, dass Du Ihnen nicht weiterhelfen kannst. Antworte auf Deutsch.")
 
 Settings.llm = llm
 Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-m3")
@@ -70,7 +72,7 @@ if st.session_state.messages[-1]["role"] != "bot":
             st.write(response.response)
 
             # Quellenangaben ausgeben
-            for node in response.source_nodes:
+            for node in response.source_nodes[:5]:
                 st.write(f"S. {node.metadata['page_label']}, Datei {node.metadata['file_path']}")
             
             # An Verlauf anhängen
